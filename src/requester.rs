ed12134ddf7809e8a4a81e657f56ab48f33662b0
Martijang <seungwoogo1123@gmail.com>
2026-06-27 07:27:30 +0800

    get/post function implemented

--- src/requester.rs
+++ src/requester.rs
@@ -1,7 +1,7 @@
 use reqwest::Client;
 use anyhow::{Result, Ok};
 
-use capibara::request::RequestResult;
+use capibara::request::{Method, RequestResult};
 
 #[derive(Debug)]
 pub struct Requester{
@@ -13,15 +13,31 @@ impl Requester{
         Self{ client: Client::new() }
     }
 
-    pub fn request(&self) -> Result<RequestResult>{
-        todo!("")
+    pub async fn request(&self, url: &str, method: &Option<Method>) -> Result<RequestResult>{
+        if let Some(method) = method{
+            match method{
+                Method::Get => {
+                    let res = self.get(url).await?;
+                    Ok(RequestResult { status: res.status, body: res.body })
+                },
+                Method::Post => {
+                    let res = self.post(url).await?;
+                    Ok(RequestResult { status: res.status, body: res.body })
+                }
+            }
+        }else{
+            let res = self.get(url).await?;
+            Ok(RequestResult { status: res.status, body: res.body })
+        }
     }
 
-    fn get(&self) -> Result<RequestResult>{
-        todo!("")
+    async fn get(&self, url: &str) -> Result<RequestResult>{
+        let res = self.client.get(url).send().await?;
+        Ok(RequestResult { status: res.status(), body: res.text().await? })
     }
 
-    fn post(&self) -> Result<RequestResult>{
-        todo!("")
+    async fn post(&self, url: &str) -> Result<RequestResult>{
+        let res = self.client.post(url).send().await?;
+        Ok(RequestResult { status: res.status(), body: res.text().await? })
     }
 }
