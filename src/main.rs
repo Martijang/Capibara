58f8b2562c2e8fef685bcb7ce7fcdf1ff5fbe373
Martijang <seungwoogo1123@gmail.com>
2026-06-28 07:36:45 +0800

    main function uses manully initalized runtime

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,15 @@ mod banner;
 
 use app::App;
 
-#[tokio::main]
-async fn main(){
-    match App::new().run().await{
-        Ok(_) => {},
-        Err(e) => eprintln!("{e}")
-    }
+fn main() -> anyhow::Result<()>{
+    let app = App::new();
+    let runtime = app.runtime_init()?;
+
+    runtime.block_on(async {
+        match app.run().await{
+            Ok(_) => {},
+            Err(e) => eprintln!("{e}")
+        }
+    });
+    anyhow::Ok(())
 }
