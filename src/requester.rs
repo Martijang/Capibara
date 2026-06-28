deb2ec658e73ff8051e1079768792dcdb04666ee
Martijang <seungwoogo1123@gmail.com>
2026-06-28 15:08:41 +0800

    formatted with cargo fmt

--- src/requester.rs
+++ src/requester.rs
@@ -1,43 +1,60 @@
+use anyhow::{Ok, Result};
 use reqwest::Client;
-use anyhow::{Result, Ok};
 
 use capibara::request::{Method, RequestResult};
 
 #[derive(Debug)]
-pub struct Requester{
-    client: Client
+pub struct Requester {
+    client: Client,
 }
 
-impl Requester{
-    pub fn new() -> Self{
-        Self{ client: Client::new() }
-    }
-
-    pub async fn request(&self, url: &str, method: &Option<Method>) -> Result<RequestResult>{
-        if let Some(method) = method{
-            match method{
-                Method::Get => {
-                    let res = self.get(url).await?;
-                    Ok(RequestResult { status: res.status, body: res.body })
-                },
-                Method::Post => {
-                    let res = self.post(url).await?;
-                    Ok(RequestResult { status: res.status, body: res.body })
-                }
-            }
-        }else{
-            let res = self.get(url).await?;
-            Ok(RequestResult { status: res.status, body: res.body })
+impl Requester {
+    pub fn new() -> Self {
+        Self {
+            client: Client::new(),
         }
     }
 
-    async fn get(&self, url: &str) -> Result<RequestResult>{
-        let res = self.client.get(url).send().await?;
-        Ok(RequestResult { status: res.status(), body: res.text().await? })
+    pub async fn request(&self, url: &str, method: &Option<Method>) -> Result<RequestResult> {
+        if let Some(method) = method {
+            match method {
+                Method::Get => {
+                    let res = self.get(url).await?;
+                    Ok(RequestResult {
+                        status: res.status,
+                        body: res.body,
+                    })
+                }
+                Method::Post => {
+                    let res = self.post(url).await?;
+                    Ok(RequestResult {
+                        status: res.status,
+                        body: res.body,
+                    })
+                }
+            }
+        } else {
+            let res = self.get(url).await?;
+            Ok(RequestResult {
+                status: res.status,
+                body: res.body,
+            })
+        }
     }
 
-    async fn post(&self, url: &str) -> Result<RequestResult>{
+    async fn get(&self, url: &str) -> Result<RequestResult> {
+        let res = self.client.get(url).send().await?;
+        Ok(RequestResult {
+            status: res.status(),
+            body: res.text().await?,
+        })
+    }
+
+    async fn post(&self, url: &str) -> Result<RequestResult> {
         let res = self.client.post(url).send().await?;
-        Ok(RequestResult { status: res.status(), body: res.text().await? })
+        Ok(RequestResult {
+            status: res.status(),
+            body: res.text().await?,
+        })
     }
 }
