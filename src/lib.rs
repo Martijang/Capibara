b56100e880b88f9c5ccb5b97a51f4510fbb284ba
Martijang <seungwoogo1123@gmail.com>
2026-06-28 15:08:34 +0800

    formatted with cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,16 @@
-
-pub mod request{
+pub mod request {
     use clap::ValueEnum;
     use reqwest::StatusCode;
+
     #[derive(Debug, Clone, Copy, ValueEnum)]
-    pub enum Method{
+    pub enum Method {
         Get,
         Post,
     }
 
-
     #[derive(Debug)]
-    pub struct RequestResult{
+    pub struct RequestResult {
         pub status: StatusCode,
-        pub body: String
+        pub body: String,
     }
 }
